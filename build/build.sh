c6951e020527f395483be4efe13f52822c2d33eb
Jaiden Nowak <windowsxp519@gmail.com>
2023-01-31 09:36:34 -0500

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -18,7 +18,7 @@ do
         "Build Beta")
             echo "Building Beta release"
             echo ""
-            /bin/bash /NeXTSPACE-OS/common/mkpackages.sh
+            /bin/bash /NeXTSPACE-OS/release/common/mkpackages.sh
 	    livecd-creator --config /NeXTSPACE-OS/release/beta/nextspace.ks --title "nxtbeta" --fslabel "nextspaceos_rel_beta"
             break
             ;;
